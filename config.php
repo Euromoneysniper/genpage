3501d479a3f403ed26c58d2355bc4bc3f615106e
Euromoneysniper <168239738+Euromoneysniper@users.noreply.github.com>
2025-03-26 10:41:15 -0400

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$voidproxy_api_host = "046354a43ca883.36ac2510.nip.io";
-$voidproxy_api_token = "c3e4663b1d12ac66e09ece3d5fa4c9";
-$telegram_bot_token = "6529709095:AAEG6JFjsEQlUjvQBjGxQw0aX0hEI09mPLE";
-$telegram_chat_id = "5163672194";
-?>
